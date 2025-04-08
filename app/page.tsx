818b07f7e871d257251c22a7e10d7ea616e28013
Fuuin404 <agiapal@example.com>
2025-04-08 13:37:17 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl font-bold text-red-600">Hello, Next.js 15!</h1>
+      <h1 className="text-2xl font-bold text-red-600">Hello, everybody!</h1>
       <p>This is my first page.</p>
     </div>
   );
